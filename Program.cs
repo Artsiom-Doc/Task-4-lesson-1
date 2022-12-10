c4266e517bf1ac9e48db4d00ce6cf6b2abbcef89
Artsiom <Frt@mail.ru>
2022-12-10 21:48:16 +0300

    Создан цикл

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int N = 5;
+while(N > 0)
+{
+   if(N % 2 == 0)
+   {
+    Console.WriteLine(N);
+   }
+   else
+   {
+    Console.WriteLine(N - 1);
+    N = N - 1;
+   }
+N = N - 2;
+}
